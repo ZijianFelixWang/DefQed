3f902dff83ddb2554df18495eac2fc4034f56f6e
ZijianWang <felix_wzj@yahoo.com>
2022-05-10 07:10:36 +0800

    Update Program.cs

--- DefQed/Program.cs
+++ DefQed/Program.cs
@@ -8,7 +8,6 @@
 using System;
 using System.Text;
 using System.Runtime.InteropServices;
-//using Terminal.Gui
 using DefQed.Core;
 
 namespace DefQed
@@ -23,14 +22,7 @@ namespace DefQed
             Console.ResetColor();
 
             Console.OutputEncoding = Encoding.Unicode;
-            //Console.WriteLine($"OS: {RuntimeInformation.OSDescription}");
             LogConsole.Log(LogLevel.Information, $"OS: {RuntimeInformation.OSDescription}");
-
-            //if (args.Length > 0)
-            //{
-            //    Console.WriteLine("Warning: Command line arguments not supported in this version. Press enter to continue.");
-            //    _ = Console.ReadLine();
-            //}
 
             if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
@@ -59,42 +51,6 @@ namespace DefQed
         private static void MainApp(string arg)
         {
             Console.Title = "DefQed version 0.01";
-            //{
-            //    X = 1,
-            //    Y = 1,
-            //    Width = Dim.Fill() - 2,
-            //    Height = Dim.Fill() - 2
-            //};
-
-            //            MenuBar Menu = new(new MenuBarItem[]
-            //            {
-            //                new("_File", new MenuItem[]
-            //                {
-            //                    new("_Load XML", "Load XML documented statement to prove.", CurrentJob.LoadXMLUI, shortcut: Key.CtrlMask | Key.M),
-            //                    new("_Renew Instance", "Dispose the current job and start a new one.", CurrentJob.RenewInstanceUI, shortcut: Key.CtrlMask | Key.R),
-            //                    new("_Quit", "Exit the program.", CurrentJob.QuitUI, shortcut: Key.CtrlMask | Key.Q)
-            //                }),
-            //                new("_Proof", new MenuItem[]
-            //                {
-            //                    new("_Start", "Begin proving.", CurrentJob.PerformProof, shortcut: Key.CtrlMask | Key.S),
-            //                    new("_Pause", "Pause current proof or resume a paused proof.", CurrentJob.PauseResumeUI,shortcut: Key.CtrlMask | Key.P),
-            //                    new("_Dispose", "Dispose current prood.", CurrentJob.DisposeProofUI, shortcut: Key.CtrlMask | Key.D),
-            //                }),
-            //                new("_Configuration", new MenuItem[]
-            //                {
-            //                    new("Set _Timeout", "Configure the timeout deadline of the proof.", CurrentJob.SetTimeoutUI, shortcut: Key.CtrlMask | Key.T),
-            //                    new("Set Lo_g Level", "Configure the log level.", CurrentJob.SetLogLevelUI, shortcut: Key.CtrlMask | Key.G),
-            //                    new("_Insert Into KBase", "For diagnostic use only.", CurrentJob.KBaseInsertRowUI, shortcut: Key.CtrlMask | Key.K),
-            //#if __ALLOW_SERIALIZE_DIAGNOSTIC_BRACKETS__
-            //                    new("Seriali_ze Diagnostic Brackets", "Should be removed after debug", CurrentJob.SerializeDiagnosticBrackets, shortcut: Key.CtrlMask | Key.Z)
-            //#endif
-            //                }),
-            //                new("_Help", new MenuItem[]
-            //                {
-            //                    new("D_ocumentation", "This will be available somewhen later...", () => { _ = MessageBox.Query("Not implemented...", "No documentation available in this version."); }, shortcut: Key.CtrlMask | Key.O),
-            //                    new("_About", "Version information", () => { _ = MessageBox.Query("DefQed", "Version 0.01\nCopyright Zijian Felix Wang. All rights reserved.\nEmail: felix_wzj@yahoo.com\n This should be licensed under MIT license."); }, shortcut: Key.CtrlMask | Key.V)
-            //                })
-            //            });
 
 #if __SERIALIZE_DEBUG_JSON__
             CurrentJob.SerializeDiagnosticBrackets();
